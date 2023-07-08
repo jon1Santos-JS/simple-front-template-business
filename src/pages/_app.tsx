f3b6a33f6c1561478bcfff9fcc13f5f9dc581f70
João Paulo dos Santos <42220755+jon1Santos-JS@users.noreply.github.com>
2023-07-08 10:51:31 -0300

    Update _app.tsx
    fixing show navigation bar conditional

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,16 @@ import '../styles/sass/index.scss';
 import { Montserrat } from '@next/font/google';
 import NavigationBar from '../components/NavigationBar';
 import NextNProgress from 'nextjs-progressbar';
+import { useRouter } from 'next/router';
 
 const montserrat = Montserrat();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    const displayTopNavigationBar = !Component.name.includes('PageError') ? (
+    const router = useRouter();
+    const displayTopNavigationBar = router.asPath !== '/404' || router.asPath !== '/500' ? (
         <NavigationBar navigationClassName="c-navigation c-top-navigation l-bg-primary-white l-light-shadow" />
     ) : null;
-    const displayBottomNavigationBar = !Component.name.includes('PageError') ? (
+    const displayBottomNavigationBar = router.asPath !== '/404' || router.asPath !== '/500' ? (
         <NavigationBar navigationClassName="c-navigation c-bottom-navigation l-bg-primary-white" />
     ) : null;
 
